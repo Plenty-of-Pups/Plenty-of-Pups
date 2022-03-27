a4da58f11f5ed8b4bf6a14444209313f1e2d7b99
Iris Han <iris.han818@gmail.com>
2022-03-27 18:32:59 -0500

    Updated NewDogProfileScreen

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import { View } from "react-native";
 import LoginScreen from "./app/screens/LoginScreen";
 import DiscoveryTabScreen from "./app/screens/DiscoveryTabScreen";
 import CreateNewUserScreen from "./app/screens/CreateNewUserScreen";
+import NewDogProfileScreen from "./app/screens/NewDogProfileScreen";
 
 export default function App() {
   // return <LoginScreen />;
-  return <CreateNewUserScreen />;
+  return <NewDogProfileScreen />;
 }
--- app/screens/NewDogProfileScreen.js
+++ app/screens/NewDogProfileScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import * as Yup from "yup";
 
 import {
@@ -9,6 +9,7 @@ import {
   SubmitButton,
 } from "../components/forms";
 import Screen from "../components/Screen";
+import FormImagePicker from "../components/forms/FormImagePicker";
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().min(1).label("Name"),
@@ -17,10 +18,60 @@ const validationSchema = Yup.object().shape({
   weight: Yup.number().required().min(1).label("Weight"),
   //will implement datetimepicker if time permits
   age: Yup.number().required().min(),
+  images: Yup.array().min(1, "Please select at least one image."),
 });
 
-function NewDogProfileScreen(props) {
-  return <div></div>;
-}
+const categories = [
+  { label: "Female", value: 1 },
+  { label: "Male", value: 2 },
+];
 
+function NewDogProfileScreen(props) {
+  return (
+    <Screen style={styles.container}>
+      <Text style={[styles.text]}>Next, tell us a little about your pup</Text>
+      <AppForm
+        initialValues={{
+          name: "",
+          breed: "",
+          gender: null,
+          weight: "",
+          age: "",
+          images: [],
+        }}
+        onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
+      >
+        <FormImagePicker name="images" />
+        <AppFormField maxLength={50} name="name" placeholder="Name" />
+        <AppFormField maxLength={50} name="breed" placeholder="Breed" />
+        <AppFormField
+          keyboardtype="numeric"
+          maxLength={3}
+          name="weight"
+          placeholder="Weight (lb)"
+        />
+        <AppFormPicker items={categories} name="gender" placeholder="Gender" />
+        <AppFormField
+          keyboardtype="numeric"
+          maxLength={3}
+          name="age"
+          placeholder="Age (years - if under 1 year old put 1)"
+        />
+        <SubmitButton title="Continue" />
+      </AppForm>
+    </Screen>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    justifyContent: "flex-end",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
 export default NewDogProfileScreen;
