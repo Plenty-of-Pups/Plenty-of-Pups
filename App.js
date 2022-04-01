3448b785e2680faed73170791b8b4b6176158fb3
Ethan Chen <chengox12@gmail.com>
2022-04-01 14:08:54 -0500

    added PreferencesScreen.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import DiscoveryTabScreen from "./app/screens/DiscoveryTabScreen";
 import PremiumCard from "./app/components/premiumCard";
 import PremiumScreen from "./app/screens/PremiumScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import PreferencesScreen from "./app/screens/PreferencesScreen";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import MessageItem from "./app/components/MessageItem";
@@ -15,7 +16,8 @@ export default function App() {
   //return <PremiumScreen />;
   //return <MessagesScreen />;
   //return <DiscoveryTabScreen />;
-  return <SettingScreen />
+  //return <SettingScreen />
+  return <PreferencesScreen />
 
 
 
---
+++ app/screens/PreferencesScreen.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
+
+import Screen from '../components/Screen';
+import colors from '../config/colors';
+import Icon from '../components/Icon';
+import AppText from '../components/AppText';
+
+const menuItems = [
+    {
+        title: "Text Size",
+        icon: {
+            name: "format-size",
+            backgroundColor: colors.white,
+            iconColor: colors.greytext
+        }
+    },
+    {
+        title: "Screen Reader",
+        icon: {
+            name: "cellphone-sound",
+            backgroundColor: colors.white,
+            iconColor: colors.greytext
+        }
+    }
+]
+
+function PreferencesScreen(props) {
+    return (
+        <View style={styles.backgroundContainer}>
+            <Screen>
+
+                <View style={styles.container}>
+                    <AppText style={styles.title}>Accessibility</AppText>
+                    <FlatList
+                        data={menuItems}
+                        keyExtractor={menuItem => menuItem.title}
+                        renderItem={({ item }) =>
+                            <MessageItem
+                                title={item.title}
+                                ImageComponent={
+                                    <Icon name={item.icon.name}
+                                        backgroundColor={item.icon.backgroundColor}
+                                        iconColor={item.icon.iconColor} />
+                                } />
+                        }
+                    />
+                </View>
+            </Screen>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        borderRadius: 15,
+        backgroundColor: colors.white,
+        marginHorizontal: 15,
+        marginVertical: 10,
+    },
+    backgroundContainer: {
+        backgroundColor: colors.lightgraybackground,
+        flex: 1,
+    },
+    title: {
+        paddingTop: 5,
+        paddingLeft: 5,
+        fontWeight: "500"
+    }
+});
+
+export default PreferencesScreen;
