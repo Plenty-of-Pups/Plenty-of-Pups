3e99c6158ec956d503887388db5a3e0f90845ed0
Ethan Chen <chengox12@gmail.com>
2022-04-01 13:56:46 -0500

    Remove TopScreen

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import DiscoveryTabScreen from "./app/screens/DiscoveryTabScreen";
 import PremiumCard from "./app/components/premiumCard";
 import PremiumScreen from "./app/screens/PremiumScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
-import TopScreen from "./app/components/topScreen";
+//import TopScreen from "./app/components/topScreen";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import MessageItem from "./app/components/MessageItem";
