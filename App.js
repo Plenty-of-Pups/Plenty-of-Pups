58b8b4d7f6e514ced70e808d4064238bc4d384bf
gabygabyte <78988305+gabygabyte@users.noreply.github.com>
2022-03-31 20:13:20 -0500

    Update App.js

--- App.js
+++ App.js
@@ -5,11 +5,13 @@ import DiscoveryTabScreen from "./app/screens/DiscoveryTabScreen";
 import PremiumCard from "./app/components/premiumCard";
 import PremiumScreen from "./app/screens/PremiumScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
-import TopScreen from "./app/components/topScreen";
+//import TopScreen from "./app/components/topScreen";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import MessageItem from "./app/components/MessageItem";
 import SettingScreen from "./app/screens/SettingScreen";
+
+
 
 export default function App() {
   //return <LoginScreen />;
