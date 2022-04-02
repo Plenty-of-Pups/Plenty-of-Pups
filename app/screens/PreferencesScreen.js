2eea06a9357bbe562687e276e6d85ebbdf9c9361
Ethan Chen <chengox12@gmail.com>
2022-04-02 18:56:12 -0500

    update

--- app/screens/PreferencesScreen.js
+++ app/screens/PreferencesScreen.js
@@ -3,18 +3,20 @@ import Slider from '@react-native-community/slider';
 import Screen from '../components/Screen';
 import { StyleSheet, View, FlatList } from 'react-native';
 import colors from '../config/colors';
+import AppText from '../components/AppText';
 
 function PreferencesScreen(props) {
   return(
     <View style={styles.backgroundContainer}>
     <Screen>
         <View style={styles.container}>
+        <AppText style={styles.title}>Match Preferences</AppText>
   <Slider
-    style={{width: 200, height: 40}}
+    style={{width: 400, height: 40}}
     minimumValue={0}
     maximumValue={1}
-    minimumTrackTintColor="#FFFFFF"
-    maximumTrackTintColor="#000000"
+    minimumTrackTintColor="#F17C87"
+    maximumTrackTintColor="#EBEBEB"
   />
   </View>
   </Screen>
@@ -23,8 +25,17 @@ function PreferencesScreen(props) {
 
 const styles = StyleSheet.create({
   backgroundContainer: {
-      backgroundColor: colors.lightgraybackground,
+      backgroundColor: colors.white,
       flex: 1,
-  }
+      padding: 10, 
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  title: {
+    paddingTop: 5,
+    textAlign: 'center',
+    fontWeight: 'bold'
+    
+}
 });
 export default PreferencesScreen;
