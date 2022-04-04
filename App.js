501cf79358031ed12761a6da76d0286d902176cc
Iris Han <iris.han818@gmail.com>
2022-04-04 03:20:49 -0500

    Added additional required screens (blank)

--- App.js
+++ App.js
@@ -18,11 +18,10 @@ import EditDogProfileScreen from "./app/screens/EditDogProfileScreen";
 
 export default function App() {
   // return <LoginScreen />;
-  // return <PremiumScreen />;
-  // return <MessagesScreen />;
-  // return <DiscoveryTabScreen />;
   // return <CreateNewUserScreen />;
   // return <NewDogProfileScreen />;
+  // return <DiscoveryTabScreen />;
+  // return <MessagesScreen />;
   return <EditUserProfileScreen />;
   //return <SettingScreen />;
   //return <EditDogProfileScreen />;
