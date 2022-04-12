5b1e30323a48bcc4a3e9d487597f3a44a364c0d4
Ethan Chen <chengox12@gmail.com>
2022-04-12 11:38:32 -0500

    PreferecnesScreen
    Updated to input

--- app/screens/PreferencesScreen.js
+++ app/screens/PreferencesScreen.js
@@ -6,6 +6,7 @@ import colors from '../config/colors';
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 import { boolean } from 'yup';
 import { Touchable } from 'react-native-web';
+import NumericInput from 'react-native-numeric-input';
 
 
 
@@ -14,7 +15,7 @@ const PreferencesScreen = () => {
 const[sliderValue, setSliderValue] = useState(50);
 
   return (
-    <View style={styles.background}>
+    <ScrollView style={styles.background}>
 
         <View style={styles.container}>
           <Text style={styles.header}>Match Preferences</Text>
@@ -75,34 +76,72 @@ const[sliderValue, setSliderValue] = useState(50);
         </View>
 
         <View style={styles.section}>
-        <Text style={styles.title}>Dog Size</Text>
-        <Text style={styles.title2}>{sliderValue} lb</Text>
-        <Slider 
-          maximumValue={100}
-          minimumValue={0}
-          minimumTrackTintColor="#F17C87"
-          maximumTrackTintColor="#EBEBEB"
-          step={1}
-          value={sliderValue}
-          onValueChange={(sliderValue) => setSliderValue(sliderValue)}
-        />
+        <Text style={styles.title}>Dog Size (lb)</Text>
+        <View style={{flexDirection: 'row', paddingBottom: 20, justifyContent: 'space-around'}}>
+        <Text style={styles.title3}>From</Text>
+        <NumericInput 
+            value={0} 
+            onChange={value => ({value})} 
+            onLimitReached={(isMax,msg) => console.log(isMax,msg)}
+            totalWidth={120} 
+            totalHeight={40} 
+            step={1}
+            valueType='real'
+            rounded 
+            textColor='#F17C87' 
+            iconStyle={{ color: 'white' }} 
+            rightButtonBackgroundColor='#EA3788' 
+            leftButtonBackgroundColor='#E56B70'/>
+            <Text style={styles.title3}>to</Text>
+            <NumericInput 
+            value={0} 
+            onChange={value => ({value})} 
+            totalWidth={120} 
+            totalHeight={40} 
+            step={1}
+            valueType='real'
+            rounded 
+            textColor='#F17C87' 
+            iconStyle={{ color: 'white' }} 
+            rightButtonBackgroundColor='#EA3788' 
+            leftButtonBackgroundColor='#E56B70'/>
+          </View>
           <View style={{flexDirection: 'row', alignItems: 'center', paddingTop: 15}}>
            <View style={{flex: 1, height: 2, backgroundColor: '#D8D8D8'}} />
           </View>
         </View>
 
         <View style={styles.section}>
-        <Text style={styles.title}>Dog Age</Text>
-        <Text style={styles.title2}>Age {sliderValue}</Text>
-        <Slider 
-          maximumValue={100}
-          minimumValue={0}
-          minimumTrackTintColor="#F17C87"
-          maximumTrackTintColor="#EBEBEB"
-          step={1}
-          value={sliderValue}
-          onValueChange={(sliderValue) => setSliderValue(sliderValue)}
-        />
+        <Text style={styles.title}>Dog Age (Years)</Text>
+        <View style={{flexDirection: 'row', paddingBottom: 20, justifyContent: 'space-around'}}>
+        <Text style={styles.title3}>From</Text>
+        <NumericInput 
+            value={0} 
+            onChange={value => ({value})} 
+            onLimitReached={(isMax,msg) => console.log(isMax,msg)}
+            totalWidth={120} 
+            totalHeight={40} 
+            step={1}
+            valueType='real'
+            rounded 
+            textColor='#F17C87' 
+            iconStyle={{ color: 'white' }} 
+            rightButtonBackgroundColor='#EA3788' 
+            leftButtonBackgroundColor='#E56B70'/>
+            <Text style={styles.title3}>to</Text>
+            <NumericInput 
+            value={0} 
+            onChange={value => ({value})} 
+            totalWidth={120} 
+            totalHeight={40} 
+            step={1}
+            valueType='real'
+            rounded 
+            textColor='#F17C87' 
+            iconStyle={{ color: 'white' }} 
+            rightButtonBackgroundColor='#EA3788' 
+            leftButtonBackgroundColor='#E56B70'/>
+          </View>
           <View style={{flexDirection: 'row', alignItems: 'center', paddingTop: 15}}>
            <View style={{flex: 1, height: 2, backgroundColor: '#D8D8D8'}} />
           </View>
@@ -167,23 +206,42 @@ const[sliderValue, setSliderValue] = useState(50);
         </View>
 
         <View style={styles.section}>
-        <Text style={styles.title}>User Age</Text>
-        <Text style={styles.title2}>Age {sliderValue}</Text>
-        <Slider 
-          maximumValue={100}
-          minimumValue={0}
-          minimumTrackTintColor="#F17C87"
-          maximumTrackTintColor="#EBEBEB"
-          step={1}
-          value={sliderValue}
-          onValueChange={(sliderValue) => setSliderValue(sliderValue)}
-        />
+        <Text style={styles.title}>User Age (Years)</Text>
+        <View style={{flexDirection: 'row', paddingBottom: 20, justifyContent: 'space-around'}}>
+        <Text style={styles.title3}>From</Text>
+        <NumericInput 
+            value={0} 
+            onChange={value => ({value})} 
+            onLimitReached={(isMax,msg) => console.log(isMax,msg)}
+            totalWidth={120} 
+            totalHeight={40} 
+            step={1}
+            valueType='real'
+            rounded 
+            textColor='#F17C87' 
+            iconStyle={{ color: 'white' }} 
+            rightButtonBackgroundColor='#EA3788' 
+            leftButtonBackgroundColor='#E56B70'/>
+            <Text style={styles.title3}>to</Text>
+            <NumericInput 
+            value={0} 
+            onChange={value => ({value})} 
+            totalWidth={120} 
+            totalHeight={40} 
+            step={1}
+            valueType='real'
+            rounded 
+            textColor='#F17C87' 
+            iconStyle={{ color: 'white' }} 
+            rightButtonBackgroundColor='#EA3788' 
+            leftButtonBackgroundColor='#E56B70'/>
+          </View>
           <View style={{flexDirection: 'row', alignItems: 'center', paddingTop: 10}}>
            <View style={{flex: 1, height: 2, backgroundColor: '#D8D8D8'}} />
           </View>
         </View>
 
-        </View>
+        </ScrollView>
   );
 };
 
@@ -246,6 +304,12 @@ const styles = StyleSheet.create({
     fontSize: 18,
     textAlign: 'left',
     direction: 'ltr'
+  },
+  title3: {
+    color: 'black',
+    fontSize: 18, 
+    fontWeight: '400',
+    paddingVertical: 10
   },
 
 });
