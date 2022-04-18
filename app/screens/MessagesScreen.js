8be3d0b900139f482616c609c9da49c0a591e4d8
Iris Han <iris.han818@gmail.com>
2022-04-17 19:58:35 -0500

    Minor alignment/styling updates to MessagesScreen.js

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,119 +1,126 @@
-import React, { useState } from 'react';
-import { FlatList, View, StyleSheet, Text, Platform, StatusBar } from 'react-native';
-
-
-
-
+import React, { useState } from "react";
+import {
+  FlatList,
+  View,
+  StyleSheet,
+  Text,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 import Constants from "expo-constants";
-import MessageItem from '../components/MessageItem';
-import ListItemSeparator from '../components/ListItemSeparator';
-import ListItemDeleteAction from '../components/ListItemDeleteAction';
-import PicIcon from '../components/PicIcon';
-import TopNav from '../components/TopNav';
-
+import MessageItem from "../components/MessageItem";
+import ListItemSeparator from "../components/ListItemSeparator";
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
+import PicIcon from "../components/PicIcon";
+import TopNav from "../components/TopNav";
 
 const initialMessages = [
-    {
-        id: 1,
-        name: 'Aurora',
-        message: 'Hi',
-        time: '11:11 AM',
-        image: require('../assets/girl1.png'),
-        numMessages: 2
+  {
+    id: 1,
+    name: "Aurora",
+    message: "Hi",
+    time: "11:11 AM",
+    image: require("../assets/girl1.png"),
+    numMessages: 2,
+  },
+  {
+    id: 2,
+    name: "Bob",
+    message: "Hello",
+    time: "4:20 PM",
+    image: {
+      uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg",
     },
-    {
-        id: 2,
-        name: 'Bob',
-        message: 'Hello',
-        time: '4:20 PM',
-        image: { uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim.jpg' },
-        numMessages: 1
+    numMessages: 1,
+  },
+  {
+    id: 3,
+    name: "Tom",
+    message: "Hello",
+    time: "5:20 PM",
+    image: require("../assets/girl2.png"),
+    numMessages: 5,
+  },
+  {
+    id: 4,
+    name: "Angelina",
+    message: "Hsjdflksdlfjlsdkfjlkdflksdj;asdjfl;",
+    time: "2:11 PM",
+    image: {
+      uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/4.jpg",
     },
-    {
-        id: 3,
-        name: 'Tom',
-        message: 'Hello',
-        time: '5:20 PM',
-        image: require('../assets/girl2.png'),
-        numMessages: 5
-    },
-    {
-        id: 4,
-        name: 'Angelina',
-        message: 'Hsjdflksdlfjlsdkfjlkdflksdj;asdjfl;',
-        time: '2:11 PM',
-        image: { uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/4.jpg' },
-        numMessages: 0
-    },
-]
+    numMessages: 0,
+  },
+];
 
 function MessagesScreen(props) {
-    const [messages, setMessages] = useState(initialMessages);
-    const [refreshing, setRefreshing] = useState(false);
+  const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
 
+  const handleDelete = (message) => {
+    //delete the message from messages
+    setMessages(messages.filter((m) => m.id !== message.id));
+  };
 
-    const handleDelete = (message) => {
-        //delete the message from messages
-        setMessages(messages.filter((m) => m.id !== message.id));
-    };
-
-    return (
-        <View View paddingTop={Constants.statusBarHeight}>
-            <TopNav title="Matches" />
-            <FlatList
-
-                horizontal
-
-                data={messages}
-                keyExtractor={message => message.id.toString()}
-                renderItem={({ item }) => (
-                    <PicIcon
-                        image={item.image}
-                        humanProfile={"HumanProfileScreen"} />
-                )}>
-
-            </FlatList>
-            <FlatList
-                ListHeaderComponent={() => <Text>Messages</Text>}
-                data={messages}
-                keyExtractor={message => message.id.toString()}
-                renderItem={({ item }) => (
-                    <MessageItem
-                        id={item.id}
-                        name={item.name}
-                        message={item.message}
-                        image={item.image}
-                        time={item.time}
-                        numMessages={item.numMessages}
-                        onPress={() => console.log("Message selected", item)}
-                        renderRightActions={() =>
-                            <ListItemDeleteAction onPress={() => handleDelete(item)} />}
-                    />
-                )}
-                ItemSeparatorComponent={ListItemSeparator}
-                refreshing={refreshing}
-                onRefresh={() => {
-                    setMessages([
-                        {
-                            id: 2,
-                            name: 'Aurora',
-                            message: 'YOOO',
-                            time: '1:23 PM',
-                            image: require('../assets/girl2.png')
-                        },
-                    ])
-                }}
-            />
-
-
-        </View>
-    );
+  return (
+    <View View paddingTop={Constants.statusBarHeight}>
+      <TopNav title="Matches" />
+      <View style={{ paddingLeft: 6 }}>
+        <FlatList
+          horizontal
+          data={messages}
+          keyExtractor={(message) => message.id.toString()}
+          renderItem={({ item }) => (
+            <PicIcon image={item.image} humanProfile={"HumanProfileScreen"} />
+          )}
+        ></FlatList>
+      </View>
+      <FlatList
+        ListHeaderComponent={() => (
+          <View style={{ paddingLeft: 10 }}>
+            <Text style={styles.text}>Messages</Text>
+          </View>
+        )}
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <MessageItem
+            id={item.id}
+            name={item.name}
+            message={item.message}
+            image={item.image}
+            time={item.time}
+            numMessages={item.numMessages}
+            onPress={() => console.log("Message selected", item)}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
+          />
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 2,
+              name: "Aurora",
+              message: "YOOO",
+              time: "1:23 PM",
+              image: require("../assets/girl2.png"),
+            },
+          ]);
+        }}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-
-
+  text: {
+    fontSize: 18,
+    fontFamily: "Avenir-Medium",
+  },
 });
 
 export default MessagesScreen;
