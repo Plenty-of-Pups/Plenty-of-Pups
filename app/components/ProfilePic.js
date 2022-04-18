f339c4e70301db3903f70c9019058def6decf878
Iris Han <iris.han818@gmail.com>
2022-04-17 19:32:03 -0500

    Updated ProfilePic.js

--- app/components/ProfilePic.js
+++ app/components/ProfilePic.js
@@ -1,68 +1,59 @@
 import React from "react";
-import { View, StyleSheet, Image, Text } from 'react-native';
+import { View, StyleSheet, Image, Text } from "react-native";
 
-
-import ListItemSeparator from '../components/ListItemSeparator';
-import colors from '../config/colors';
-
-
-
-
-
+import ListItemSeparator from "../components/ListItemSeparator";
+import colors from "../config/colors";
 
 function ProfilePic({ image, name }) {
-    return (
-
-        <View style={styles.container}>
-            <Image style={styles.image} source={image} />
-
-        </View>
-
-
-
-
-    );
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={image} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        padding: 5,
-        marginVertical: 10,
-        borderColor: colors.greytext
-    },
-    detailsContainer: {
-        flex: 1,
-        justifyContent: 'center'
-    },
-    image: {
-        width: 250,
-        height: 250,
-        borderRadius: 180,
-        marginLeft: 60,
-    },
-    message: {
-        color: colors.greytext,
-    },
-    name: {
-        fontSize: 18,
-        color: colors.black,
-        fontWeight: "600",
-        marginBottom: 5,
-    },
-    row: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    text: {
-        color: colors.greytext,
-    }
-
-
+  container: {
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    flex: 1,
+    flexDirection: "row",
+    padding: 5,
+    marginVertical: 10,
+    borderColor: colors.greytext,
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  image: {
+    width: 250,
+    height: 250,
+    borderRadius: 180,
+    alignContent: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    marginBottom: 30,
+  },
+  message: {
+    color: colors.greytext,
+  },
+  name: {
+    fontSize: 18,
+    color: colors.black,
+    fontWeight: "600",
+    marginBottom: 5,
+    fontFamily: "Avenir-Medium",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  text: {
+    color: colors.greytext,
+  },
 });
-
-
 
 export default ProfilePic;
