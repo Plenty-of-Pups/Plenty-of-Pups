6f5e5f58b8b6cf69b24bb8e00b65d22a27a0d2e5
Iris Han <iris.han818@gmail.com>
2022-04-19 02:23:29 -0500

    Alignment and image updates made on YourDogScreen.js

--- app/components/ProfileItem.js
+++ app/components/ProfileItem.js
@@ -1,42 +1,38 @@
-import React from 'react';
-import styles from '../assets/styles';
-import { Text, View, Image} from 'react-native';
-import Demo from '../assets/data/demo.js';
-const ProfileItem = ({
-  age,
-  info1,
-  location,
-  name,
-  dog,
-  dogImage
-}=Demo[8]) => {
+import React from "react";
+import styles from "../assets/styles";
+import { Text, View, Image } from "react-native";
+import Demo from "../assets/data/demo.js";
+const ProfileItem = (
+  { age, info1, location, name, dog, dogImage } = Demo[8]
+) => {
   return (
     <View style={styles.containerProfileItem}>
-
-      <Text style={styles.name}>{name}, {age}</Text>
+      <Text style={styles.name}>
+        {name}, {age}
+      </Text>
       <Text style={styles.descriptionProfileItem}>{location}</Text>
-      
-      <View style={{flexDirection: 'row', alignItems: 'center'}}>
-      <View style={{flex: 1, height: 1, backgroundColor: '#D8D8D8'}} />
-      </View>
 
-        <View style={styles.dogInfo}>
-        <Text style={styles.bioContent}>Bio: </Text>
-        </View>
-        <Text style={styles.infoContent}>{info1}</Text>
-
-      
-      <View style={styles.info}>
-      <Text style={styles.iconProfile}></Text>
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <View style={{ flex: 1, height: 1, backgroundColor: "#D8D8D8" }} />
       </View>
 
       <View style={styles.dogInfo}>
-      <Text style={styles.bioContent}>My pup </Text>
+        <Text style={styles.bioContent}>Bio: </Text>
+      </View>
+      <Text style={styles.infoContent}>{info1}</Text>
+
+      <View style={styles.info}>
+        <Text style={styles.iconProfile}></Text>
       </View>
 
-      <Image source ={dogImage} style={styles.circle}></Image>
-      <View style={styles.info}>
-      <Text style={styles.dogName}>{dog}</Text>
+      <View style={styles.dogInfo}>
+        <Text style={styles.bioContent}>My pup </Text>
+      </View>
+      <View style={styles.pupInfo}>
+        <Image source={dogImage} style={styles.circle}></Image>
+      </View>
+      <View style={styles.dogNameInfo}>
+        <Text style={styles.dogName}>{dog}</Text>
       </View>
     </View>
   );
--- app/screens/YourDogScreen.js
+++ app/screens/YourDogScreen.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import styles from '../assets/styles';
+import React from "react";
+import styles from "../assets/styles";
 import {
   ScrollView,
   View,
   Text,
   ImageBackground,
   TouchableOpacity,
-  Image
-} from 'react-native';
-import ProfileItem from '../components/DogProfileItem.js';
-import Demo from '../assets/data/demo.js';
+  Image,
+} from "react-native";
+import ProfileItem from "../components/DogProfileItem.js";
+import Demo from "../assets/data/demo.js";
 import Constants from "expo-constants";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import BackNavigation from '../components/BackNavigation';
-import colors from '../config/colors';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import BackNavigation from "../components/BackNavigation";
+import colors from "../config/colors";
 
 const YourDogScreen = () => {
   const {
@@ -27,16 +27,18 @@ const YourDogScreen = () => {
     hobby,
     snack,
     gender,
-  } = Demo[1];
+  } = Demo[9];
 
   return (
-
-    <ScrollView style={styles.containerProfile} paddingTop={Constants.statusBarHeight}>
-      <BackNavigation title={"View Pets Profile"} rightIconColor={colors.beigebackground} />
-      <ImageBackground source={image} style={styles.photo}>
-
-
-      </ImageBackground>
+    <ScrollView
+      style={styles.containerProfile}
+      paddingTop={Constants.statusBarHeight}
+    >
+      <BackNavigation
+        title={"View Pets Profile"}
+        rightIconColor={colors.beigebackground}
+      />
+      <ImageBackground source={image} style={styles.photo}></ImageBackground>
 
       <ProfileItem
         dogName={dogName}
